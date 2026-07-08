31479ecd33cd4d255aa69a2d802f5b20cdd589af
JulianCandatenLubas <juliancandatenlubas@gmail.com>
2026-07-07 21:31:13 -0300

    .

--- semana-2/README.md
+++ semana-2/README.md
@@ -1,3 +1,1 @@
 # sctec2
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/JulianCandatenLubas/sctec2)
--- semana-2/index.js
+++ semana-2/index.js
@@ -1,4 +1,7 @@
 // run `node index.js` in the terminal
 
 console.log("Olá Mundo");
-//1634165464
+
+let nomeAluno = "Julian"; //valor pode ser alterado
+var idadeAluno = 32; //método mais antigo
+const curso = "JavaScript"; //valor não pode ser alterado
